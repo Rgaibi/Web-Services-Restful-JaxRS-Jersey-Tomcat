271513c4e6efcfc4849119d8cba8bc44a6b9bae8
karim rgaibi <karimrgaibi@gmail.com>
2025-09-15 16:24:40 +0200

    Resource creation

--- src/main/java/com/ServicesWebREST/messenger/resources/MessageResource.java
+++ src/main/java/com/ServicesWebREST/messenger/resources/MessageResource.java
@@ -1,5 +1,17 @@
 package com.ServicesWebREST.messenger.resources;
 
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+
+@Path("messages")
 public class MessageResource {
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getMessages() {
+		return "HELLOW WORLD";
+	}
 
 }
