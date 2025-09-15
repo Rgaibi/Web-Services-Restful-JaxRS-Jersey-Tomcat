67769a77998b8a1eae8362b1c5a33b96b3417f18
karim rgaibi <karimrgaibi@gmail.com>
2025-09-15 20:13:01 +0200

    Returning XML response

--- src/main/java/com/ServicesWebREST/messenger/resources/MessageResource.java
+++ src/main/java/com/ServicesWebREST/messenger/resources/MessageResource.java
@@ -1,17 +1,25 @@
 package com.ServicesWebREST.messenger.resources;
 
+import java.util.List;
+
+import com.ServicesWebREST.messenger.model.Message;
+import com.ServicesWebREST.messenger.service.MessageService;
+
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
 
-@Path("messages")
+@Path("/messages")
 public class MessageResource {
+	
+	MessageService messageservice = new MessageService();
+	
 	@GET
-	@Produces(MediaType.TEXT_PLAIN)
-	public String getMessages() {
-		return "HELLOW WORLD";
+	@Produces(MediaType.APPLICATION_XML)
+	public List<Message> getMessages() {
+		return messageservice.getAllMesages();
 	}
 
 }
